2ecd2fbe28e6464b87f899fd103dba8773dee462
Alex-Karmazin <karmazinalex@gmail.com>
2022-12-29 18:13:43 +0200

    some refactoring and prs access modification from list to dict with name as key

--- longevity2reporter.py
+++ longevity2reporter.py
@@ -30,8 +30,7 @@ class Reporter(CravatReport):
         self.db_cursor = self.db_conn.cursor()
         outpath = f'{self.savepath}.longevity2.html'
         self.outfile = open(outpath, 'w', encoding='utf-8')
-        cur_path = str(Path(__file__).parent)
-        self.template = Template(filename=str(Path(__file__).parent)+"/template.html")
+        self.template = Template(filename=str(Path(__file__).parent / "template.html"))
 
 
     def write_table(self, name, json_fields = [], sort_field = "", sort_revers = False):
@@ -56,27 +55,64 @@ class Reporter(CravatReport):
                         if len(lst) == 0:
                             tmp[item[0]] = ""
                         else:
-                            tmp[item[0]] = lst[0]
+                            tmp[item[0]] = lst
                     else:
                         tmp[item[0]] = row[i]
                 res.append(tmp)
-            self.data[name] = res
+            return res
         except Exception as e:
             print("Warning:", e)
 
 
+    def write_table_to_dict(self, name, key_field, json_fields = [], sort_field = "", sort_revers = False):
+        if key_field in json_fields:
+            raise ValueError("key_field should not be in json_fields")
+
+        sort_sql = ""
+        if sort_field != "":
+            sort_sql = " ORDER BY " + sort_field
+            if sort_revers:
+                sort_sql = sort_sql+" DESC"
+            else:
+                sort_sql = sort_sql+" ASC"
+
+        self.db_cursor.execute("SELECT * FROM "+name+sort_sql)
+        rows = self.db_cursor.fetchall()
+        res = dict()
+
+        col_value = None
+        for row in rows:
+            tmp = {}
+            for i, item in enumerate(self.db_cursor.description):
+                col_name = item[0]
+                if col_name in json_fields:
+                    lst = json.loads(row[i])
+                    if len(lst) == 0:
+                        tmp[col_name] = ""
+                    else:
+                        tmp[col_name] = lst
+                else:
+                    tmp[col_name] = row[i]
+                if col_name == key_field:
+                    col_value = row[i]
+            if col_value is None:
+                raise ValueError("key_field is not in list of available fields")
+            res[col_value] = tmp
+        return res
+
+
     def write_data(self):
         # self.data = {"test1":[1,2,3], "test2":["aa", "bbb", "cccc"]}
-        self.data = {}
-        self.write_table("prs", [], "id", True)
-        self.write_table("longevitymap", ["conflicted_rows", "description"], "weight", False)
-        self.write_table("cancer", [], "id", True)
-        self.write_table("coronary", [], "weight", False)
-        self.write_table("drugs", [], "id", True)
+        data = {}
+        data["prs"] = self.write_table_to_dict("prs", "name")
+        data["longevitymap"] = self.write_table("longevitymap", ["conflicted_rows", "description"], "weight", False)
+        data["cancer"] = self.write_table("cancer", [], "id", True)
+        data["coronary"] = self.write_table("coronary", [], "weight", False)
+        data["drugs"] = self.write_table("drugs", [], "id", True)
+        self.outfile.write(self.template.render(data=data))
 
 
     def end(self):
-        self.outfile.write(self.template.render(data=self.data))
         self.outfile.close()
         return Path(self.outfile.name).resolve()
 
